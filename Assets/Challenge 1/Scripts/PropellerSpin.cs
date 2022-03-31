cf54c96d9ebc1545297c1df84bb3ff61b3c2fc64
Kai-6 <89489271+Kai-6@users.noreply.github.com>
2022-03-30 21:54:28 -0700

    Done and Done
    Challenge completed as well

---
+++ Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+  private int RPM = 360/60;
+  private float propSpeed = 2600;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, (propSpeed*RPM) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+  public int num = 20;
+  public GameObject obstacle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < num+1; i++) {
+          GameObject obs = Object.Instantiate(obstacle, new Vector3(Random.Range(-8,8), 0, map(i, 0, num, 25, 180)), Quaternion.Euler(0,180,0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+float map (float value, float from1, float to1, float from2, float to2) {
+    return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+}
+}
